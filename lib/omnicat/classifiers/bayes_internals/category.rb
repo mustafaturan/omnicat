708aedbd280bf4bcc9c84ae02a359772b3ab0fae
Mustafa Turan <mustafaturan.net@gmail.com>
2013-06-19 02:27:38 +0300

    added prior to bayes category

--- lib/omnicat/classifiers/bayes_internals/category.rb
+++ lib/omnicat/classifiers/bayes_internals/category.rb
@@ -2,10 +2,11 @@ module OmniCat
   module Classifiers
     module BayesInternals
       class Category < ::OmniCat::Base
-        attr_accessor :doc_count, :tokens, :token_count
+        attr_accessor :doc_count, :prior, :tokens, :token_count
 
         def initialize(category_hash = {})
           self.doc_count = category_hash[:doc_count].to_i
+          self.prior = category_hash[:prior].to_f
           self.tokens = category_hash[:tokens] || {}
           self.token_count = category_hash[:token_count].to_i
         end
--- lib/test/unit/hash_test.rb
+++ lib/test/unit/hash_test.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper'))
 
 class TestHash < Test::Unit::TestCase
   def test_to_hash
-    categories_hash = { "pos" => { doc_count: 0, tokens: {}, token_count: 0 } }
+    categories_hash = { "pos" => { doc_count: 0, prior: 0.0, tokens: {}, token_count: 0 } }
     categories = OmniCat::Hash.new
     categories["pos"] = OmniCat::Classifiers::BayesInternals::Category.new(categories_hash["pos"])
     assert_equal(categories_hash, categories.to_hash)
