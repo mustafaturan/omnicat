096237f1a39d6e0df6af29df1b25eb6ebe095403
Mustafa Turan <mustafaturan.net@gmail.com>
2013-06-29 14:21:54 +0300

    added tests for OmniCat::Classifiers::Bayes methods

--- lib/test/unit/bayes_test.rb
+++ lib/test/unit/bayes_test.rb
@@ -31,6 +31,15 @@ class TestBayes < Test::Unit::TestCase
     assert_raise(StandardError) { @bayes.add_category "neutral" }
   end
 
+  def test_add_categories
+    @bayes.add_categories ["neutral", "positive", "negative"]
+    assert_not_nil(@bayes.categories["neutral"])
+    assert_equal(
+      ["neutral", "positive", "negative"],
+      @bayes.categories.keys
+    )
+  end
+
   def test_train_valid_category
     @bayes.add_category "neutral"
     @bayes.train "neutral", "how are you?? : :| :) ;-) :("
@@ -45,6 +54,16 @@ class TestBayes < Test::Unit::TestCase
     assert_equal(
       9,
       @bayes.categories["neutral"].token_count
+    )
+  end
+
+  def test_train_batch
+    @bayes.add_category "positive"
+    @bayes.train_batch "positive", ["good job ever", "valid syntax",
+      "best moments of my life"]
+    assert_equal(
+      3,
+      @bayes.categories["positive"].doc_count
     )
   end
 
@@ -72,6 +91,28 @@ class TestBayes < Test::Unit::TestCase
     assert_equal(
       "negative",
       @bayes.classify("bad words").category[:name]
+    )
+  end
+
+  def test_classify_batch
+    @bayes.add_category "positive"
+    @bayes.add_category "negative"
+    @bayes.train_batch "positive", ["good job ever", "valid syntax",
+      "best moments of my life"]
+    @bayes.train_batch("negative", ["bad work", "awfull day", "never liked it"])
+    results = @bayes.classify_batch(
+      ["good sytanx research", "bad words"]
+    )
+
+    assert_equal(2, results.count)
+
+    assert_equal(
+      "positive",
+      results[0].category[:name]
+    )
+    assert_equal(
+      "negative",
+      results[1].category[:name]
     )
   end
 
