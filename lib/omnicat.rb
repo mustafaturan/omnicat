58190efad4af1911cc941965c7d062efa152ec91
Mustafa Turan <mustafaturan.net@gmail.com>
2013-07-06 15:34:31 +0300

    requirements change for omnicat

--- lib/omnicat.rb
+++ lib/omnicat.rb
@@ -1,7 +1,22 @@
 require File.dirname(__FILE__) + '/omnicat/version'
-require File.dirname(__FILE__) + '/omnicat/string'
+require File.dirname(__FILE__) + '/omnicat/configuration'
 require File.dirname(__FILE__) + '/omnicat/array'
 require File.dirname(__FILE__) + '/omnicat/hash'
 require File.dirname(__FILE__) + '/omnicat/base'
+require File.dirname(__FILE__) + '/omnicat/doc'
 require File.dirname(__FILE__) + '/omnicat/result'
-require File.dirname(__FILE__) + '/omnicat/bayes'
+require File.dirname(__FILE__) + '/omnicat/classifier'
+
+module OmniCat
+  def self.config
+    OmniCat::Configuration.instance
+  end
+
+  def self.configure
+    yield config
+  end
+
+  def self.logger
+    config.logger
+  end
+end
