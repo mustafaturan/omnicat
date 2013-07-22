9fd60b3c1bb381e715ee80b34e9797521dff50cd
Mustafa Turan <mustafaturan.net@gmail.com>
2013-07-22 23:54:06 +0300

    auto train configuration param added

--- lib/omnicat/configuration.rb
+++ lib/omnicat/configuration.rb
@@ -7,7 +7,7 @@ module OmniCat
   class Configuration
     include Singleton
     attr_accessor :logger
-    attr_accessor :exclude_tokens, :logger, :token_patterns
+    attr_accessor :auto_train, :exclude_tokens, :logger, :token_patterns
 
     def self.default_logger
       logger = Logger.new(STDOUT)
@@ -16,6 +16,7 @@ module OmniCat
     end
 
     @@defaults = {
+      auto_train: false,
       exclude_tokens: ['a','about','across','after','all','almost','also','am','among','an','and','are','as','at','be','because','been','by','did','do','does','else','ever','every','for','from','get','got','had','has','have','he','her','hers','him','his','how','however','i','if','in','into','is','it','its','just','least','let','may','me','might','most','must','my','of','often','on','only','or','other','our','own','rather','said','say','says','she','should','since','so','some','than','that','the','their','them','then','there','these','they','this','tis','to','too','twas','us','wants','was','we','were','what','when','where','which','while','who','whom','will','with','would','yet','you','your'],
       logger: default_logger,
       token_patterns: {
