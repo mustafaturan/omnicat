f41424dee70c3808071c9269eeedd57af97b58dd
Mustafa Turan <mustafaturan.net@gmail.com>
2013-07-22 23:54:58 +0300

    name attr added to category

--- lib/omnicat/classifiers/strategy_internals/category.rb
+++ lib/omnicat/classifiers/strategy_internals/category.rb
@@ -4,11 +4,12 @@ module OmniCat
   module Classifiers
     module StrategyInternals
       class Category < ::OmniCat::Base
-        attr_accessor :doc_count, :docs, :tokens, :token_count
+        attr_accessor :name, :doc_count, :docs, :tokens, :token_count
 
         def initialize(category_hash = {})
           @doc_count = category_hash[:doc_count].to_i
           @docs = category_hash[:docs] || {}
+          @name = category_hash[:name] || nil
           @tokens = category_hash[:tokens] || {}
           @token_count = category_hash[:token_count].to_i
         end
